791e44fdc49ccf248ca5652a1876409d64e6e6b0
M1naT04ka <bakhtin.t2@mail.ru>
2026-05-08 13:16:35 +0300

    Рефакторинг: вынесен метод printMessage

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main{
     public static void main(String[] args) {
+        printMessage();
+    }
+    
+    static void printMessage() {
         System.out.println("Это добавлено в ветке dev");
         System.out.println("Новая строка 1");
     }
