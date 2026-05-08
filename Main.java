b34b7c2dc3ac19dfad2c2092bec010b8a6a135ac
M1naT04ka <bakhtin.t2@mail.ru>
2026-05-08 13:16:21 +0300

    Добавлен вывод двух строк

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке dev");
+        System.out.println("Это добавлено в ветке dev");
+        System.out.println("Новая строка 1");
     }
 }
